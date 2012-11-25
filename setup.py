88f62e9c4c9a5a09b89d013718bd01b5eef7ebe0
aisensiy <aisensiy@gmail.com>
2012-11-25 10:58:47 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+requires = [
+
+    'flask',
+    'PIL'
+
+    ]
+
+setup(name='rosi-view',
+      version='0.1',
+      author='aisensiy',
+      author_email='aisensiy@gmail.com',
+      install_requires=requires)
